6eb4adaac1e687bf6b2380f0b89ecfebef447cc9
Carl LeBlond <carl.leblond@iup.edu>
2014-09-05 20:37:54 -0400

    cleaned up css

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2014083000;
+$plugin->version   = 2014090500;
 $plugin->requires  = 2012062500;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_easyonamejs';
