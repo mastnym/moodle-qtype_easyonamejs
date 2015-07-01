1de72a7bf98de1c561a0873a8c96b644018ac26c
Kathrin <kathrin.osswald@uni-ulm.de>
2015-07-01 15:12:11 +0200

    Added ChemAxon Logo to display right under the editor
    As the external license for MarvinJS is required to use your plugin and
    a requirement to use the license is that the logo from ChemAxon should
    be placed on the site, I added it directly to your plugin.

--- renderer.php
+++ renderer.php
@@ -18,7 +18,7 @@
  *
  * @package    qtype
  * @subpackage easyonamejs
- * @copyright  2014 onwards Carl LeBlond 
+ * @copyright  2014 onwards Carl LeBlond
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
@@ -26,15 +26,15 @@ $generatedfeedback = "";
 /**
  * Generates the output for easyonamejs questions.
  *
- * @copyright  2014 onwards Carl LeBlond 
+ * @copyright  2014 onwards Carl LeBlond
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class qtype_easyonamejs_renderer extends qtype_renderer {
     public function formulation_and_controls(question_attempt $qa, question_display_options $options) {
-        global $CFG, $PAGE;
+        global $CFG, $PAGE, $OUTPUT;
         $question        = $qa->get_question();
         $questiontext    = $question->format_questiontext($qa);
-        $uniqid = uniqid();          
+        $uniqid = uniqid();
         $myanswerid      = "my_answer" . $uniqid;
         $correctanswerid = "correct_answer" . $uniqid;
         $marvinjsconfig  = get_config('qtype_easyonamejs_options');
@@ -117,6 +117,14 @@ class qtype_easyonamejs_renderer extends qtype_renderer {
             'class' => 'inputcontrol'
         ));
         $this->require_js($toreplaceid, $qa, $options->readonly, $options->correctness, $uniqid);
+        $result .= html_writer::start_tag('div', array('class'=> 'licence_logo'));
+        $result .= html_writer::start_tag('a', array('href'=> 'http://www.chemaxon.com'));
+        $result .= html_writer::empty_tag('img', array(
+        'src'=>$OUTPUT->pix_url('chemaxon', 'qtype_easyonamejs'),
+        'alt'=>'ChemAxon Licence Logo',
+        'id' => 'chemaxon'));
+        $result .= html_writer::end_tag('a');
+        $result .= html_writer::end_tag('div');
         return $result;
     }
     protected function remove_xml_tags($xmlstring, $tag) {
